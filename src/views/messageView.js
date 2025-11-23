7f0ed2285bc0a21725d36550f4af8c424b94ad89
mohamed-mahsoub <mohamedmahsob18@gmail.com>
2025-11-23 20:40:41 +0200

    inhance: remove duplcated checks for messages changes in messages vies

--- src/views/messageView.js
+++ src/views/messageView.js
@@ -1,30 +1,19 @@
 import { createElement, appendChildren } from "../utils/domHelpers.js";
 import { getIsIndecator, getMessages } from "../state/index.js";
-let lastMessageCount = 0;
 
 export const renderMessages = (state, container) => {
   const messages = getMessages(state);
-  // Only re-render if messages actually changed
-  if (messages.length === lastMessageCount && container.children.length > 0) {
-    // Check if we just need to append new messages
-    if (messages.length > lastMessageCount) {
-      const newMessages = messages.slice(lastMessageCount);
-      const newMessageElements = newMessages.map(createMessageElement);
-      appendChildren(container, newMessageElements);
-    }
-  } else {
-    // Full re-render
-    container.innerHTML = "";
-    const messageElements = messages.map(createMessageElement);
 
-    const isIndecator = getIsIndecator(state);
-    const childrenToAppend = isIndecator
-      ? [...messageElements, createIndecatorElement()]
-      : messageElements;
-    appendChildren(container, childrenToAppend);
-  }
+  // Full re-render
+  container.innerHTML = "";
+  const messageElements = messages.map(createMessageElement);
 
-  lastMessageCount = messages.length;
+  const isIndecator = getIsIndecator(state);
+  const childrenToAppend = isIndecator
+    ? [...messageElements, createIndecatorElement()]
+    : messageElements;
+  appendChildren(container, childrenToAppend);
+
   scrollToBottom(container);
 };
 
